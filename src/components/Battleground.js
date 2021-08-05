4d4a6e10dcffcb36c076ea55cb23b42acae9cdec
Robert Rousset <whyisrob@hotmail.com>
2021-08-05 19:16:26 +0800

    added a toggle between signup and login

--- src/components/Battleground.js
+++ src/components/Battleground.js
@@ -1,19 +1,46 @@
+import { useState, useEffect } from "react";
+
 export default function Battleground() {
-  function handleSingup() {}
+  const [toggle, setToggle] = useState(false);
+
+  const [submitButton, setSubmitButton] = useState("Login");
+  const [loginOrSignup, setloginOrSignup] = useState(
+    "Dont have an account yet?"
+  );
+  const [loginOrSignupButton, setloginOrSignupButton] = useState("Signup");
+
+  useEffect(() => {
+    if (toggle) {
+      setSubmitButton("Sign up");
+      setloginOrSignup("Already got an accout?");
+      setloginOrSignupButton("Login");
+    }
+    if (!toggle) {
+      setSubmitButton("Login");
+      setloginOrSignup("Dont have an account yet?");
+      setloginOrSignupButton("Signup");
+    }
+  }, [toggle]);
+
   return (
     <>
-      <div className="signin-container">
+      <div className="login-container">
         <input placeholder="Email"></input>
         <input placeholder="Password" type="password"></input>
-        <button className="button" id="signin">
-          Login
+        <button className="button" id="login">
+          {submitButton}
         </button>
 
         <p>
-          Dont have an account yet?{" "}
-          <button className="signup" onClick={handleSingup}>
+          {loginOrSignup}{" "}
+          <button
+            className="signup"
+            onClick={() => {
+              setToggle(!toggle);
+            }}
+          >
             {" "}
-            Sign up
+            {loginOrSignupButton}
           </button>
         </p>
       </div>
