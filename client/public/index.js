20994bb468cf3d691199878bcb27612cde63f876
Robert Rousset <whyisrob@hotmail.com>
2021-08-07 00:24:16 +0800

    added movement animation to the warrior sprite

--- client/public/index.js
+++ client/public/index.js
@@ -6,7 +6,58 @@ const k = kaboom({
   scale: 1,
 });
 
-loadSprite("warrior", "./img/sprites/warrior.png");
+loadSprite("warriorDown", "./img/sprites/warrior-dude/walking-down.png", {
+  sliceX: 4,
+  anims: {
+    idle: {
+      from: 0,
+      to: 0,
+    },
+    walk: {
+      from: 0,
+      to: 3,
+    },
+  },
+});
+loadSprite("warriorUp", "./img/sprites/warrior-dude/walking-up.png", {
+  sliceX: 4,
+  anims: {
+    idle: {
+      from: 0,
+      to: 0,
+    },
+    walk: {
+      from: 0,
+      to: 3,
+    },
+  },
+});
+loadSprite("warriorLeft", "./img/sprites/warrior-dude/walking-left.png", {
+  sliceX: 4,
+  anims: {
+    idle: {
+      from: 0,
+      to: 0,
+    },
+    walk: {
+      from: 0,
+      to: 3,
+    },
+  },
+});
+loadSprite("warriorRight", "./img/sprites/warrior-dude/walking-right.png", {
+  sliceX: 4,
+  anims: {
+    idle: {
+      from: 0,
+      to: 0,
+    },
+    walk: {
+      from: 0,
+      to: 3,
+    },
+  },
+});
 
 loadSprite("background", "./img/battleground.jpg");
 
@@ -34,21 +85,65 @@ scene("game", (levelIndex) => {
     scale(1),
     origin("center"),
   ]);
-  const warrior = add([sprite("warrior"), pos(100, 100), scale(0.1)]);
+  const warrior = add([sprite("warriorRight"), pos(100, 100), scale(0.2)]);
 
   add([text("0"), pos(20, 20), layer("ui"), { value: "test" }, scale(4)]);
 
-  const dirs = {
-    a: vec2(-1, 0),
-    d: vec2(1, 0),
-    w: vec2(0, -1),
-    s: vec2(0, 1),
-  };
-  for (const dir in dirs) {
-    keyDown(dir, () => {
-      warrior.move(dirs[dir].scale(SPEED));
-    });
-  }
+  // const dirs = {
+  //   a: vec2(-1, 0),
+  //   d: vec2(1, 0),
+  //   w: vec2(0, -1),
+  //   s: vec2(0, 1),
+  // };
+  // for (const dir in dirs) {
+  //   keyDown(dir, () => {
+  //     warrior.move(dirs[dir].scale(SPEED));
+  //   });
+  // }
+
+  keyPress("a", () => {
+    warrior.changeSprite("warriorLeft", { animSpeed: 3, frame: 0 });
+    warrior.play("walk");
+  });
+  keyDown("a", () => {
+    warrior.move(-SPEED, 0);
+  });
+  keyRelease("a", () => {
+    warrior.play("idle");
+  });
+
+  keyPress("s", () => {
+    warrior.changeSprite("warriorDown", { animSpeed: 3, frame: 0 });
+    warrior.play("walk");
+  });
+  keyDown("s", () => {
+    warrior.move(0, SPEED);
+  });
+  keyRelease("s", () => {
+    warrior.play("idle");
+  });
+
+  keyPress("d", () => {
+    warrior.changeSprite("warriorRight", { animSpeed: 3, frame: 0 });
+    warrior.play("walk");
+  });
+  keyDown("d", () => {
+    warrior.move(SPEED, 0);
+  });
+  keyRelease("d", () => {
+    warrior.play("idle");
+  });
+
+  keyPress("w", () => {
+    warrior.changeSprite("warriorUp", { animSpeed: 3, frame: 0 });
+    warrior.play("walk");
+  });
+  keyDown("w", () => {
+    warrior.move(0, -SPEED);
+  });
+  keyRelease("w", () => {
+    warrior.play("idle");
+  });
 
   let dashIsNotCooldown = true;
   keyPress("space", () => {
