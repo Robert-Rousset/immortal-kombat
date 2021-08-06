4fecdc6ad94267a1ece9e1f1ebe66f8c50f3a287
Robert Rousset <whyisrob@hotmail.com>
2021-08-06 23:15:22 +0800

    added attack animation in forward direction

--- client/public/index.js
+++ client/public/index.js
@@ -10,11 +10,30 @@ loadSprite("warrior", "./img/sprites/warrior.png");
 
 loadSprite("background", "./img/battleground.jpg");
 
+loadSprite("attack", "./img/attack/attackanim.png", {
+  sliceX: 4,
+  sliceY: 2,
+  anims: {
+    attack: {
+      from: 0,
+      to: 7,
+    },
+  },
+});
+
 scene("game", (levelIndex) => {
+  origin("center");
+  let ATTACK_SPEED = 250;
   let SPEED = 200;
+  let DASH = 4;
   layers([("background", "obj", "ui"), "obj"]);
 
-  const background = add([sprite("background"), pos(0, -400), scale(2)]);
+  add([
+    sprite("background"),
+    pos(width() / 2, height() / 2),
+    scale(1),
+    origin("center"),
+  ]);
   const warrior = add([sprite("warrior"), pos(100, 100), scale(0.1)]);
 
   add([text("0"), pos(20, 20), layer("ui"), { value: "test" }, scale(4)]);
@@ -30,11 +49,52 @@ scene("game", (levelIndex) => {
       warrior.move(dirs[dir].scale(SPEED));
     });
   }
-  keyPress("shift", () => {
-    SPEED = SPEED * 5;
-    wait(0.08, () => {
-      SPEED = SPEED / 5;
+
+  let dashIsNotCooldown = true;
+  keyPress("space", () => {
+    if (dashIsNotCooldown) {
+      SPEED = SPEED * DASH;
+      dashIsNotCooldown = false;
+      wait(0.08, () => {
+        SPEED = SPEED / DASH;
+        resetDashCooldown();
+      });
+    }
+  });
+  function resetDashCooldown() {
+    if (dashIsNotCooldown) {
+      wait(2, () => {
+        dashIsNotCooldown = true;
+      });
+    }
+    if (dashIsNotCooldown === false) {
+      wait(2, () => {
+        dashIsNotCooldown = true;
+      });
+    }
+  }
+
+  keyPress("up", () => {});
+
+  function spawnAttack(s) {
+    const attack = add([
+      sprite("attack", { animSpeed: 0.05, frame: 0 }),
+      pos(s),
+      scale(0.3),
+      "attack",
+    ]);
+    attack.play("attack");
+    wait(0.4, () => {
+      destroy(attack);
     });
+  }
+
+  keyPress("up", () => {
+    spawnAttack(warrior.pos.add(-20, 0));
+  });
+
+  action("attack", (s) => {
+    s.move(0, -ATTACK_SPEED);
   });
 
   warrior.action(() => {
