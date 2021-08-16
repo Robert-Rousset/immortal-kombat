976514d3e884f2796ec1052091f34ab14b55c23d
Robert Rousset <whyisrob@hotmail.com>
2021-08-16 17:26:15 +0800

    got rid of db.dropDatabase()

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-db.dropDatabase();
+
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
