90a6b541a31adde9b3704499de1853c90e5e3fcb
Robert Rousset <whyisrob@hotmail.com>
2021-09-08 14:19:08 +0800

    restarting database

--- server/server.js
+++ server/server.js
@@ -31,6 +31,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+db.dropDatabase();
+
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
